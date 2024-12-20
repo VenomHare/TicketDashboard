ef74caffb422fd0fa4d17b9d82e7bcb989c41586
Mr.VenomHare <venomhare9@gmail.com>
2024-12-20 19:47:51 +0530

    1.0.0
    Login URL

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -9,9 +9,9 @@ const LoginMobile = dynamic(() => import('@/components/pageComponents/admin/logi
 const LoginDesktop = dynamic(() => import('@/components/pageComponents/admin/login/LoginDesktop'), { ssr: false });
 
 
-const LoginURI='https://discord.com/oauth2/authorize?client_id=1311315146932621345&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fadmin%2Flogin&scope=identify+email+guilds.members.read'
+// const LoginURI='https://discord.com/oauth2/authorize?client_id=1311315146932621345&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fadmin%2Flogin&scope=identify+email+guilds.members.read'
 // const LoginURI='https://discord.com/oauth2/authorize?client_id=1311315146932621345&response_type=code&redirect_uri=https%3A%2F%2Fvdljj0vs-3000.inc1.devtunnels.ms%2Fadmin%2Flogin&scope=identify+email+guilds.members.read'
-// const LoginURI='https://discord.com/oauth2/authorize?client_id=1311315146932621345&response_type=code&redirect_uri=https%3A%2F%2Fvh-tickets.vercel.app%2Fadmin%2Flogin&scope=identify+email+guilds.members.read+guilds'
+const LoginURI='https://discord.com/oauth2/authorize?client_id=1311315146932621345&response_type=code&redirect_uri=https%3A%2F%2Fvh-tickets.vercel.app%2Fadmin%2Flogin&scope=identify+email+guilds.members.read+guilds'
 
 const Index : React.FC = () => {
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,9 @@ import dynamic from 'next/dynamic';
 const LoginMobile = dynamic(() => import('@/components/pageComponents/login/LoginMobile'), { ssr: false });
 const LoginDesktop = dynamic(() => import('@/components/pageComponents/login/LoginDesktop'), { ssr: false });
 
-export const loginURI = 'https://discord.com/oauth2/authorize?client_id=1311315146932621345&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2FloginRequest&scope=identify+email';
+// export const loginURI = 'https://discord.com/oauth2/authorize?client_id=1311315146932621345&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2FloginRequest&scope=identify+email';
 // export const loginURI = 'https://discord.com/oauth2/authorize?client_id=1311315146932621345&response_type=code&redirect_uri=https%3A%2F%2Fvdljj0vs-3000.inc1.devtunnels.ms%2Fapi%2FloginRequest&scope=identify+email';
-// export const loginURI = 'https://discord.com/oauth2/authorize?client_id=1311315146932621345&response_type=code&redirect_uri=https%3A%2F%2Fvh-tickets.vercel.app%2Fapi%2FloginRequest&scope=identify+email';
+export const loginURI = 'https://discord.com/oauth2/authorize?client_id=1311315146932621345&response_type=code&redirect_uri=https%3A%2F%2Fvh-tickets.vercel.app%2Fapi%2FloginRequest&scope=identify+email';
 export default function Home() {
   const isMobile = useMediaQuery({maxWidth: "1000px"})
 
